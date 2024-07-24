8ee25f0df9b2162b11b2c4c190e104c05234769b
misha-gupta <163374741+mg224@users.noreply.github.com>
2024-07-24 16:29:30 -0400

    Create __init__.py

---
+++ website/__init__.py
@@ -0,0 +1,36 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+
+# Initialize database
+db = SQLAlchemy()
+DB_NAME = "database.db"
+
+def create_app():
+    app = Flask(__name__)
+    app.config["SECRET_KEY"] = "9aa688fa374e4b33be6275fb9892dcc3"
+    app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{DB_NAME}"
+    db.init_app(app)
+
+    # Import auth, views, models
+    from .auth import auth
+    app.register_blueprint(auth, url_prefix="/")
+
+    from .views import views
+    app.register_blueprint(views, url_prefix="/")
+
+    from .models import User, Note
+    with app.app_context():
+        db.create_all()
+
+    # Initialize LoginManager
+    login_manager = LoginManager()
+    login_manager.login_view = "auth.login"
+    login_manager.login_message_category = "error"
+    login_manager.init_app(app)
+
+    @login_manager.user_loader
+    def load_user(id):
+        return User.query.get(int(id))
+
+    return app
